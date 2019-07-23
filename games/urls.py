51ffee84ee528a4782b4defd8bb8837c41c988da
wzrumich <wezerui@umich.edu>
2019-07-23 00:57:32 -0400

    update

---
+++ games/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.welcome, name='welcome'),   
+    path('login/', views.login, name='login'),
+    path('investment/', views.investment, name='investment'),
+    path('investmentSubmit/', views.investmentSubmit, name='investmentSubmit'),
+    path('returnedSubmit/', views.returnedSubmit, name='returnedSubmit'),
+    path('final/', views.final, name='final'),
+    path('returning/', views.returned, name='returned'),
+    path('compare/', views.compare, name='compare')
+    # path('', views.p, name='welcome'),    
+]
