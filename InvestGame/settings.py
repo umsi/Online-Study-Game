c1717be6d0fdd598cf911ac0f0ef5c8ca9772346
Ubuntu <ubuntu@ip-172-31-26-180.us-east-2.compute.internal>
2019-08-25 20:07:56 +0000

    aws congif

--- InvestGame/settings.py
+++ InvestGame/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '94wk@_!^++=19fu#*7irm*_$1dh_rugz=osfikzf890z6l@fc5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
