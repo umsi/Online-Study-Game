da4fd75449baa0f225cfab2175113bd3584cd400
Trevor Croxson <goldpbear@users.noreply.github.com>
2020-02-09 09:36:31 -0600

    Goldpbear/prod deployment (#6)
    * chore: set up elastic beanstalk deployment
    
    * chore(eb config): use dedicated AWS profile
    
    * refactor(wsgi.py): remove custom logging code
    
    * chore(ebextensions): create superuser on deploy
    
    * chore(settings): admin static dir
    
    * chore(ebextensions): run collecstatic on deploy
    
    * chore(eb config): default to deploy off master

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for InvestGame project.
+WSGI config for Online-Study-Game project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InvestGame.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
