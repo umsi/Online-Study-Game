f409e82dec212dce97ccb35e7c8250a977924a4b
goldpbear <tcroxson@gmail.com>
2020-02-11 16:28:35 -0600

    feat(admin): add CSV download for Investment model

--- games/invest_game/admin.py
+++ games/invest_game/admin.py
@@ -1,4 +1,7 @@
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from .models import InvestmentGameUser, Investment
 
@@ -8,7 +11,23 @@ class InvestmentGameUserAdmin(admin.ModelAdmin):
 
 
 class InvestmentAdmin(admin.ModelAdmin):
-    pass
+    actions = ["download_csv"]
+
+    def download_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = "attachment; filename={}.csv".format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow(getattr(obj, field) for field in field_names)
+
+        return response
+
+    download_csv.short_description = "Download CSV for selected Investment instances"
 
 
 admin.site.register(InvestmentGameUser, InvestmentGameUserAdmin)
