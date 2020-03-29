bb18ea3dab57b0ee1f697b839da1190865565940
goldpbear <tcroxson@gmail.com>
2020-03-29 09:14:22 -0500

    fix(urls.py)

--- games/invest_game/urls.py
+++ games/invest_game/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path("compare/", views.compare, name="compare"),
     path("question1/", views.question1, name="question1"),
     path("question2/", views.question2, name="question2"),
-    path("question2-5/", views.question1_5, name="question2_5"),
+    path("question2-5/", views.question2_5, name="question2_5"),
     path("question3/", views.question3, name="question3"),
     path("finish/", views.finish, name="finish"),
 ]
