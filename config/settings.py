b11a44e4c9413b00149f55fa0e5b1b1e8f9e476e
goldpbear <tcroxson@gmail.com>
2020-06-25 15:41:05 -0500

    chore(settings): default to prod mode

--- config/settings.py
+++ config/settings.py
@@ -23,8 +23,8 @@ INFO_STORE = os.path.join(BASE_DIR, "games/userdata")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "iamnotasecret")
 
-# Default to prod mode; disable with an environment variable.
-DEBUG = os.environ.get("DEBUG", False)
+# Default to prod mode; disable with the `DJANGO_DEBUG` environment variable.
+DEBUG = os.environ.get("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = ["*"]
 
