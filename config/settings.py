132996dfc4796b5a22ce29d250d3fed62c82d98b
goldpbear <tcroxson@gmail.com>
2020-06-25 15:33:32 -0500

    chore: default to prod mode

--- config/settings.py
+++ config/settings.py
@@ -23,8 +23,8 @@ INFO_STORE = os.path.join(BASE_DIR, "games/userdata")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "iamnotasecret")
 
-# Default to debug mode; disable in production with an environment variable.
-DEBUG = os.environ.get("DEBUG", True)
+# Default to prod mode; disable with an environment variable.
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = ["*"]
 
